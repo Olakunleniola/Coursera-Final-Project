75105799c6d36e17fe43f8a022cf148375786881
Olakunle Eniola <akandeola48@gmail.com>
2023-02-26 10:28:41 +0100

    I modified the changeImage.py file to capture the fullpath to the images being converted.
    These are also the .tiff image files being replaced after executing the changeImage.py script

--- changeImage.py
+++ changeImage.py
@@ -2,11 +2,11 @@
 from PIL import Image
 import os
 import send2trash
- 
-for img  in os.listdir('Final_Project_Folder/supplier-data/images/'):
+pwwd = '/mnt/c/Users/OLAKUNLE AKANDE/Downloads/COURSERA FILES/Coursera part 6 (Automating Real-World Tasks with Python)/Final_Project_Folder'
+for img  in os.listdir(pwwd + '/supplier-data/images/'):
     try:
-        im = Image.open('Final_Project_Folder/supplier-data/images/' + img).convert("RGB")
-        im.resize((600,400)).save(f"Final_Project_Folder/supplier-data/images/{img.strip('.tiff')}.jpeg")
-        send2trash.send2trash('Final_Project_Folder/supplier-data/images/' + img)
+        im = Image.open(pwwd +'/supplier-data/images/' + img).convert("RGB")
+        im.resize((600,400)).save(f"{pwwd}/supplier-data/images/{img.strip('.tiff')}.jpeg")
+        send2trash.send2trash(pwwd + '/supplier-data/images/' + img)
     except:
         pass
